0c83d6d8c801007706282a97ced1f54924b5a9fc
Byron0127 <108595829+Byron0127@users.noreply.github.com>
2025-05-06 23:32:56 -0500

    implementacion de migracion excel a bd (byron)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import AppLayout from './AppLayout';
+import SubirExcel from './pages/SubirExcel';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
@@ -28,6 +29,7 @@ function App() {
             <Route path="/agregar-actividad" element={<AgregarActividad />} />
             <Route path="/perfil" element={<Perfil />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/subir-excel" element={<SubirExcel />} />
           </Routes>
         </AppLayout>
       </AuthProvider>
